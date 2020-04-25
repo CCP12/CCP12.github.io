515c9759288fea11eb697e4286a30902d804b1ce
CCP12 <64296811+CCP12@users.noreply.github.com>
2020-04-25 02:41:38 -0400

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/kitty.jpg') {
+      myImage.setAttribute ('src','images/me.jpg');
+    } else {
+      myImage.setAttribute ('src','images/kitty.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Cats are cool, ' + myName;
+  }
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Cats are cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
